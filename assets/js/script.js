e17a79761d259772a601854baba3d560e54cd7ad
Natalie Bulger <bulgern20@gmail.com>
2022-04-04 20:34:18 -0700

    Updated display summary card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,36 @@
 
 
 
-var recipeCardContainerEl = document.querySelector("recipe-card-container");
-var recipeCardEl = document.querySelectorAll("recipe-card");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var recipeCardContainerEl = document.querySelector(".recipe-card-container");
+var recipeCardEl = document.querySelectorAll(".recipe-card");
 //function to display the fivedayforecast 
 function displaySummaryRecipeCards(repos){
 
@@ -24,29 +52,32 @@ function displaySummaryRecipeCards(repos){
 
         //Gather all the necessary information for the recipe cards
         //Gather recipe title
-        var recipeTitle = repos[i].label;
+        var recipeTitle = repos.hits[i].recipe.label;
         //Number of servings the recipe makes
-        var numberOfServings =  repos[i].yield + " Servings";
+        var numberOfServings =  repos.hits[i].recipe.yield + " Servings";
         //Number of kcal in the recipe
-        var kcalCount = repos[i].calories.toFixed(1) + " kcal";
+        var kcalCount = repos.hits[i].recipe.calories.toFixed(1) + " kcal";
         //Protein amount in grams
-        var proteinAmount = "Protein: " + repos[i].totalNutrients.procnt.quantity + " g";
-        var fatAmount = "Fat: " + repos[i].totalNutrients.fat.quantity + " g";
-        var carbAmount = "Carb: " + repos[i].totalNutrients.chocdf.quantity + " g";
-        var imagePath = repos[i].image;
+        var proteinAmount = "Protein: " + repos.hits[i].recipe.totalNutrients.PROCNT.quantity.toFixed(1) + " g";
+        var fatAmount = "Fat: " + repos.hits[i].recipe.totalNutrients.FAT.quantity.toFixed(1) + " g";
+        var carbAmount = "Carb: " + repos.hits[i].recipe.totalNutrients.CHOCDF.quantity.toFixed(1) + " g";
+        var imagePath = repos.hits[i].recipe.image;
 
         //Create the child elements
         var baseInfoContainerEl = document.createElement('div');
+        baseInfoContainerEl.classList.add("recipe-card-info");
         var recipeTitleEl = document.createElement('h3');
         var numberOfServingsEl = document.createElement('p');
         var kcalCountEl = document.createElement('p');
 
         var macroInfoContainerEl = document.createElement('div');
+        macroInfoContainerEl.classList.add("recipe-card-info");
         var proteinAmountEl = document.createElement('p');
         var fatAmountEl = document.createElement('p');
         var carbAmountEl = document.createElement('p');
 
         var imageContainerEl = document.createElement('img');
+        imageContainerEl.classList.add("recipe-card-info");
 
         //Apply the text content using the gathered information and child elements
         recipeTitleEl.textContent = recipeTitle;
@@ -70,6 +101,16 @@ function displaySummaryRecipeCards(repos){
         recipeCardEl[i].appendChild(baseInfoContainerEl);
         recipeCardEl[i].appendChild(macroInfoContainerEl);
         recipeCardEl[i].appendChild(imageContainerEl);
+
     }
 
 };
+
+recipeCardContainerEl.addEventListener("click", function(event){
+    var recipeIndex = event.target.getAttribute("data-index");
+    displayChosenRecipeCard(recipeIndex);
+});
+
+function displayChosenRecipeCard(recipeIndex) {
+
+}
